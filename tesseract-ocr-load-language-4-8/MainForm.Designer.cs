62754bc8ed167d690f498f2d83d4d9ba6707e1df
Tom Gregor <tgregor@ivsoftware.com>
2023-01-20 14:47:59 -0700

    Add project files.

---
+++ tesseract-ocr-load-language-4-8/MainForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace tesseract_ocr_load_language_4_8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadLanguage = new System.Windows.Forms.Button();
+            this.buttonOpenFolder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLoadLanguage
+            // 
+            this.buttonLoadLanguage.Location = new System.Drawing.Point(23, 23);
+            this.buttonLoadLanguage.Name = "buttonLoadLanguage";
+            this.buttonLoadLanguage.Size = new System.Drawing.Size(162, 60);
+            this.buttonLoadLanguage.TabIndex = 0;
+            this.buttonLoadLanguage.Text = "Load Language";
+            this.buttonLoadLanguage.UseVisualStyleBackColor = true;
+            // 
+            // buttonOpenFolder
+            // 
+            this.buttonOpenFolder.Location = new System.Drawing.Point(23, 113);
+            this.buttonOpenFolder.Name = "buttonOpenFolder";
+            this.buttonOpenFolder.Size = new System.Drawing.Size(162, 60);
+            this.buttonOpenFolder.TabIndex = 0;
+            this.buttonOpenFolder.Text = "Open Folder";
+            this.buttonOpenFolder.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.buttonOpenFolder);
+            this.Controls.Add(this.buttonLoadLanguage);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadLanguage;
+        private System.Windows.Forms.Button buttonOpenFolder;
+    }
+}
+
---
+++ tesseract-ocr-load-language-4-8/MainForm.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tesseract_ocr_load_language_4_8
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            buttonLoadLanguage.Click += onClickLoadLanguages;
+            buttonOpenFolder.Click += onOpenFolder;
+            _path = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                GetType().Assembly.GetName().Name,
+                "tesseract-ocr");
+            Directory.CreateDirectory(_path);
+        }
+
+        private readonly string _path;
+        // https://github.com/tesseract-ocr/tessdata/
+        private void onClickLoadLanguages(object sender, EventArgs e)
+        {
+            LoadLanguage(_path, "eng");
+        }
+        public void LoadLanguage(string folderName, string language)
+        {
+            Enabled = false;
+            try
+            {            
+                Uri uri = new Uri($"https://github.com/tesseract-ocr/tessdata/raw/main/{language}.traineddata");
+                using (var client = new HttpClient())
+                {
+                    using (var response =
+                        client
+                        .GetAsync(uri)
+                        .Result)
+                    {
+                        var bytes = response.Content.ReadAsByteArrayAsync().Result;
+                        File.WriteAllBytes(Path.Combine(folderName, $"{language}.traineddata"), bytes);
+                    }
+                }
+            }
+            finally
+            {
+                Enabled = true;
+            }
+        }
+
+        private void onOpenFolder(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", _path);
+        }
+    }
+}
